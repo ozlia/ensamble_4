9aa9a8890a7231ff7f5ffe7264246506f79f5092
Netanel Shaked <shakolnak@gmail.com>
2022-01-05 22:20:27 +0200

    appended all model decisions options, there is a bug in the indexing of predict one sample - the index that came from knn function is not exist somehow ?!

--- decision_dependent_direct_knn.py
+++ decision_dependent_direct_knn.py
@@ -1,3 +1,5 @@
+import random
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from lightgbm import LGBMClassifier
@@ -16,25 +18,31 @@ class DecisionDependentDirectKNN:
         self.train_x = []
         self.train_y = []
 
-    def fit(self, train_x, train_y):
+    def fit(self, train_x:pd.DataFrame, train_y:pd.DataFrame):
         for m in self.models:
             m.fit(train_x, train_y)
         self.knn.fit(train_x, train_y)
-        self.train_x = train_x
-        self.train_y = train_y
+        self.train_x = train_x.reset_index().drop(columns='index')
+        self.train_y = train_y.reset_index().drop(columns='index')
 
     def predict_one(self, sample):
+        import warnings
+        warnings.filterwarnings('ignore')
+
         nn = self.knn.kneighbors([sample], 5, return_distance=False)
         model_pred_score = {}
         for n in nn[0]:
             for m in self.models:
-                if m.predict([self.train_x[n]]) == self.train_y[n]:
-                    model_pred_score[m] = model_pred_score[m] + 1 if m in model_pred_score else 0
-        best_model = [(k, v) for k, v in sorted(model_pred_score.items(), key=lambda item: item[1], reverse=True)][0][0]
+                if m.predict([self.train_x.iloc[n]]) == self.train_y.iloc[n].values[0] == m.predict([sample]):
+                    model_pred_score[m] = model_pred_score[m] + 1 if m in model_pred_score else 1
+        if len(model_pred_score) == 0:
+            best_model = random.sample(self.models, 1)[0]
+        else:
+            best_model = [(k, v) for k, v in sorted(model_pred_score.items(), key=lambda item: item[1], reverse=True)][0][0]
         return best_model.predict([sample])[0]
 
-    def predict(self, test_x):
+    def predict(self, test_x:pd.DataFrame):
         predictions = []
-        for sample in test_x:
+        for index, sample in test_x.iterrows():
             predictions.append(self.predict_one(sample))
         return predictions
--- decision_independent_distance_based_knn.py
+++ decision_independent_distance_based_knn.py
@@ -1,4 +1,19 @@
-class DecisionIndependentDistanceBasedKNN:
+from decision_dependent_direct_knn import DecisionDependentDirectKNN
+import random
 
-    def __init__(self):
-        super()
+class DecisionIndependentDistanceBasedKNN(DecisionDependentDirectKNN):
+    def predict_one(self, sample):
+        nn = self.knn.kneighbors([sample], 5, return_distance=False)
+        model_pred_score = {}
+        i = 0
+        for n in nn[0]:
+            i += 1
+            for m in self.models:
+                if m.predict([self.train_x.iloc[n]]) == self.train_y.iloc[n].values[0]:
+                    rank = 1/i
+                    model_pred_score[m] = model_pred_score[m] + rank if m in model_pred_score else rank
+        if len(model_pred_score) == 0:
+            best_model = random.sample(self.models, 1)[0]
+        else:
+            best_model = [(k, v) for k, v in sorted(model_pred_score.items(), key=lambda item: item[1], reverse=True)][0][0]
+        return best_model.predict([sample])[0]
